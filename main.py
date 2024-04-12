d7053d6fd3cdfeb7808c1f5beecb7c0aae7299ce
dgana <dganashok@gmail.com>
2024-04-12 21:39:13 +0300

    Added comments

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-import argparse, os, shutil
+import argparse, os
 from datetime import datetime
 from prettytable import PrettyTable
 import pandas as pd
 
+# Importing Selenium browser drivers
 from Browser.Selenium_browser import driver as dr
 from Browser.Selenium_undetected import driver as dr_un
 from Browser.Selenium_node import driver as dr_nd
 
+# Browser class for managing profiles
 class Browser:
     def __init__(self):
         self.filename = 'list_profiles.csv'
 
     def check_file_exists(self):
+        # Check if the file exists in the directory
         return pd.read_csv(self.filename, nrows=1).shape[0] > 0 if os.path.isfile(self.filename) else False
 
     def write_to_csv(self, data):
+        # Create or write to the file
         if self.check_file_exists():
             df = pd.read_csv(self.filename)
             
+            # Check if profile name already exists and update last open time and number of opens
             if data['Name'] in df['Name'].values and 'Creat_Time' in data.keys():
                 print('There is already a profile with this name.')
             
@@ -26,13 +31,14 @@ class Browser:
                 df.loc[df['Name'] == data['Name'], 'Num_Open'] += 1
             
             else:
-                df = df._append(data, ignore_index=True)
+                df = df.append(data, ignore_index=True)
         else:
             df = pd.DataFrame([data])
 
         df.to_csv(self.filename, index=False)
 
     def add_proxy(self, proxy):
+        # Add proxy to the profile
         if self.check_file_exists():
             df = pd.read_csv(self.filename)
             if proxy[0] in df['Name'].values:
@@ -42,10 +48,12 @@ class Browser:
                 df.to_csv(self.filename, index=False)
 
     def list_profiles(self):
+        # Display profiles in a table format
         table = PrettyTable()
 
+        # Set column names
         table.field_names = ["Number", "Name", "Browser", "Proxy", "Time_Creat"]
-
+        
         df = pd.read_csv(self.filename)
         data = df.to_dict(orient='records')
         
@@ -54,7 +62,8 @@ class Browser:
         
         print(table)
 
-    def delet_profile(self, name):
+    def delete_profile(self, name):
+        # Delete a profile by name
         if self.check_file_exists():
             df = pd.read_csv(self.filename)
             if name in df['Name'].values:
@@ -62,23 +71,25 @@ class Browser:
                 df = df.drop(index_to_remove)
                 df.to_csv(self.filename, index=False)
 
-    def creat_profile(self, name_browser):
+    def create_profile(self, name_browser):
+        # Create a new profile
         if name_browser[1] in ('selenium', 'selen_unde', 'node'):
-            print(f'Creat profile ({name_browser})')
+            print(f'Create profile ({name_browser})')
             self.write_to_csv({'Name': name_browser[0], 'Browser': name_browser[1], 'Creat_Time': str(datetime.now()), 'Num_Open': 0})
         else:
-            print('No name browser')
+            print('No valid browser name provided')
 
     def start_profiles(self, name):
+        # Start a profile
         df = pd.read_csv(self.filename)
         browser = df.loc[df['Name'] == name, 'Browser'].values
-        Type_Proxy = df.loc[df['Name'] == name, 'Type_Proxy'].values
-        Host_Proxy = df.loc[df['Name'] == name, 'Host_Proxy'].values
+        type_proxy = df.loc[df['Name'] == name, 'Type_Proxy'].values
+        host_proxy = df.loc[df['Name'] == name, 'Host_Proxy'].values
 
         if browser == 'selenium':
             self.write_to_csv({'Name': name})
             driver = dr()
-            driver.creat_profile(name, {'type': Type_Proxy, 'host': Host_Proxy})
+            driver.create_profile(name, {'type': type_proxy, 'host': host_proxy})
             driver.driver_start()
             input('Stop profile (Yes?)')
             self.driver_stop()
@@ -86,7 +97,7 @@ class Browser:
         elif browser == 'selen_unde':
             self.write_to_csv({'Name': name})
             driver = dr_un()
-            driver.creat_profile(name, {'type': Type_Proxy, 'host': Host_Proxy})
+            driver.create_profile(name, {'type': type_proxy, 'host': host_proxy})
             driver.driver_start()
             input('Stop profile (Yes?)')
             self.driver_stop()
@@ -94,12 +105,13 @@ class Browser:
         elif browser == 'node':
             self.write_to_csv({'Name': name})
             driver = dr_nd()
-            driver.creat_profile(name)
+            driver.create_profile(name)
             driver.driver_start()
             input('Stop profile (Yes?)')
             self.driver_stop()
 
     def export_cookies(self, exp):
+        # Export cookies for a profile
         df = pd.read_csv(self.filename)
         browser = df.loc[df['Name'] == exp[0], 'Browser'].values
 
@@ -116,20 +128,21 @@ class Browser:
             driver.get_cookies(exp[0], exp[1])
             
 if '__main__' == __name__:
+    # Parsing command line arguments
     parser = argparse.ArgumentParser(description='Command Information.')
-    parser.add_argument('-c', '--creat', nargs='+', help='creat profile, [name, browser(selenium or selen_unde or node)]')
+    parser.add_argument('-c', '--creat', nargs='+', help='create profile, [name, browser(selenium or selen_unde or node)]')
     parser.add_argument('-s', '--start', help='Start profile')
-    parser.add_argument('-d', '--delet', help='Delet profile')
-    parser.add_argument('-l', '--list', action='store_const', const=True, default=False, help='list profiles')
-    parser.add_argument('-p', '--proxy', nargs='+', help='add proxy profile [name profile, type proxy, ip:port]')
-    parser.add_argument('-ec', '--export_cookies', nargs='+', help='export cookies profile (name profile, path save file)')
+    parser.add_argument('-d', '--delete', help='Delete profile')
+    parser.add_argument('-l', '--list', action='store_const', const=True, default=False, help='List profiles')
+    parser.add_argument('-p', '--proxy', nargs='+', help='Add proxy to profile [name profile, type proxy, ip:port]')
+    parser.add_argument('-ec', '--export_cookies', nargs='+', help='Export cookies for profile (name profile, path save file)')
 
     args = parser.parse_args()
     Browser = Browser()
 
     if args.proxy: Browser.add_proxy(args.proxy)
     if args.list: Browser.list_profiles()
-    if args.delet: Browser.delet_profile(args.delet)
-    if args.creat: Browser.creat_profile(args.creat)
+    if args.delete: Browser.delete_profile(args.delete)
+    if args.create: Browser.create_profile(args.create)
     if args.start: Browser.start_profiles(args.start)
     if args.export_cookies: Browser.export_cookies(args.export_cookies)
