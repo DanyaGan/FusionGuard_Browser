612661ff476fe4f51a130455c8956b27669e06dd
dgana <dganashok@gmail.com>
2024-04-18 20:59:34 +0300

    Added comments

--- Browser/Selenium_browser.py
+++ Browser/Selenium_browser.py
@@ -7,19 +7,21 @@ from typing import Dict, List
 
 class WebDriverManager:
     def __init__(self):
+        # Initialize WebDriverManager object
         self.path_user_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'profiles')
         self.driver: webdriver.Chrome = None
         self.chrome_options: webdriver.ChromeOptions = None
-        self.user_agents = self.load_user_agents()
+        self.user_agents = self.load_user_agents()  # Load user agents upon initialization
 
     def load_user_agents(self) -> List[str]:
-        # Ideally, this should be loaded from an external file or a config
+        # Load user agents list (Ideally, this should be loaded from an external file or a config)
         return [
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
             # Add other user agents here
         ]
 
     def create_profile(self, name: str, proxy: Dict[str, str]):
+        # Create Chrome profile with given name and proxy settings
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument(f"user-agent={random.choice(self.user_agents)}")
         self.chrome_options.add_argument(f"--user-data-dir={self.path_user_data}")
@@ -32,14 +34,17 @@ class WebDriverManager:
             self.chrome_options.add_argument(f'--proxy-server=https://{proxy["host"]}')
 
     def start_driver(self):
+        # Start Chrome driver with configured options
         if self.chrome_options:
             self.driver = webdriver.Chrome(options=self.chrome_options)
 
     def stop_driver(self):
+        # Quit Chrome driver if it's running
         if self.driver:
             self.driver.quit()
 
     def get_cookies(self, name_profile: str, path_file: str):
+        # Retrieve cookies for a specific profile and save them to a file
         cpath = os.path.join(self.path_user_data, name_profile, 'Network', 'Cookies')
         cookies = []
         try:
@@ -57,12 +62,13 @@ class WebDriverManager:
                         'expires': expires_utc, 'secure': bool(is_secure), 'httponly': bool(is_httponly)
                     }
                     cookies.append(cookie)
-                f.write(str(cookies))
+                f.write(str(cookies))  # Write cookies to file
         except sqlite3.Error as e:
             print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
+    # Main block
     web_driver_manager = WebDriverManager()
-    web_driver_manager.start_driver()
+    web_driver_manager.start_driver()  # Start the web driver
     # More logic can be added here to use the web driver
-    web_driver_manager.stop_driver()
+    web_driver_manager.stop_driver()  # Stop the web driver
